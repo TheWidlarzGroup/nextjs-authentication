784b159d09a6d720bed89d8a14ed3723f8babf01
Nsquik <46610139+Nsquik@users.noreply.github.com>
2021-03-03 23:47:04 +0100

    Add register proxy path

---
+++ pages/api/register.tsx
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { headers, body } = req
+
+  try {
+    const { data, headers: returnedHeaders } = await axios.post(
+      'http://localhost:3001/auth/register',
+      body,
+      {
+        headers,
+      }
+    )
+    //  Update headers on requester using headers from Node.js server response
+    Object.entries(returnedHeaders).forEach((keyArr) =>
+      res.setHeader(keyArr[0], keyArr[1] as string)
+    )
+    res.status(200).json(data)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
