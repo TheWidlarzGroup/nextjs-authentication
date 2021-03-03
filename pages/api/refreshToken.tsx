0bc90b5911290cbef7c33aec4abfa29c8ae0f7d7
Nsquik <46610139+Nsquik@users.noreply.github.com>
2021-03-03 23:11:35 +0100

    Add refreshToken proxy path

---
+++ pages/api/refreshToken.tsx
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { headers } = req
+  try {
+    const { data, headers: returnedHeaders } = await axios.post(
+      'http://localhost:3001/auth/refresh-token', // refresh token Node.js server path
+      undefined,
+      {
+        headers,
+      }
+    )
+
+    //  Update headers on requester using headers from Node.js server response
+    Object.keys(returnedHeaders).forEach((key) => res.setHeader(key, returnedHeaders[key]))
+
+    res.status(200).json(data)
+  } catch (e) {
+    throw new Error(e)
+  }
+}
