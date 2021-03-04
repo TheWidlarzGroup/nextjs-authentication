8834edda144bd0797c2ea75ce37ccd4cf3cf66fa
Nsquik <46610139+Nsquik@users.noreply.github.com>
2021-03-04 02:41:09 +0100

    Wrapp app with provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/styles.css'
 import styled from 'styled-components'
 import tw from 'twin.macro'
+import { Provider } from 'react-redux'
+import { wrapper, store } from '../lib/store'
 import { Header } from '../components/Header'
 
 export const PageWrapper = styled.main`
@@ -10,12 +12,14 @@ export const PageWrapper = styled.main`
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PageWrapper>
-      <Header>
-        <Component {...pageProps} />
-      </Header>
-    </PageWrapper>
+    <Provider store={store}>
+      <PageWrapper>
+        <Header>
+          <Component {...pageProps} />
+        </Header>
+      </PageWrapper>
+    </Provider>
   )
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp)
