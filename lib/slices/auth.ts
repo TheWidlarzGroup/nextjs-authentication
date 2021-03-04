80e5d63805332049f5fe91be8f7ab25a728186ff
Nsquik <46610139+Nsquik@users.noreply.github.com>
2021-03-04 01:56:20 +0100

    Init async actions

--- lib/slices/auth.ts
+++ lib/slices/auth.ts
@@ -1,9 +1,57 @@
-import { createSlice, SerializedError } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction, SerializedError } from '@reduxjs/toolkit'
+import axios from '../axios'
 
 export enum AuthStates {
   IDLE = 'idle',
   LOADING = 'loading',
 }
+
+export const fetchUser = createAsyncThunk('auth/me', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get<{ name: string; email: string; type: string }>('api/me')
+
+    return response.data
+  } catch (error) {
+    return thunkAPI.rejectWithValue({ error: error.message })
+  }
+})
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (credentials: { email: string; password: string }, thunkAPI) => {
+    try {
+      const response = await axios.post<{ accessToken: string }>('api/login', credentials)
+      const refetch = await axios.get<{ name: string }>('api/me', {
+        headers: { Authorization: `Bearer ${response.data.accessToken}` },
+      })
+      return { accessToken: response.data.accessToken, me: { name: refetch.data.name } }
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.message })
+    }
+  }
+)
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials: { email: string; password: string; name: string }, thunkAPI) => {
+    try {
+      const response = await axios.post<{ accessToken: string }>('api/register', credentials)
+      const refetch = await axios.get<{ name: string }>('api/me', {
+        headers: { Authorization: `Bearer ${response.data.accessToken}` },
+      })
+      return { accessToken: response.data.accessToken, me: { name: refetch.data.name } }
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.message })
+    }
+  }
+)
+export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    const response = await axios.delete<{ accessToken: string }>('api/logout')
+    return response.data
+  } catch (error) {
+    return thunkAPI.rejectWithValue({ error: error.message })
+  }
+})
 
 export interface AuthSliceState {
   accessToken: string
@@ -23,8 +71,44 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState: internalInitialState,
   reducers: {
+    updateAccessToken(state: AuthSliceState, action: PayloadAction<{ token: string }>) {
+      state.accessToken = action.payload.token
+    },
     reset: () => internalInitialState,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.accessToken = action.payload.accessToken
+      state.me = action.payload.me
+      state.loading = AuthStates.IDLE
+    })
+    builder.addCase(login.rejected, (state, action) => {
+      state = { ...internalInitialState, error: action.error }
+      throw new Error(action.error.message)
+    })
+    builder.addCase(logout.pending, (state) => {
+      state.loading = AuthStates.LOADING
+    })
+    builder.addCase(logout.fulfilled, (_state) => internalInitialState)
+    builder.addCase(login.pending, (state) => {
+      state.loading = AuthStates.LOADING
+    })
+    builder.addCase(register.fulfilled, (state, action) => {
+      state.accessToken = action.payload.accessToken
+      state.me = action.payload.me
+      state.loading = AuthStates.IDLE
+    })
+    builder.addCase(register.rejected, (state, action) => {
+      state.error = action.error
+    })
+    builder.addCase(fetchUser.rejected, (state, action) => {
+      state = { ...internalInitialState, error: action.error }
+      throw new Error(action.error.message)
+    })
+    builder.addCase(fetchUser.fulfilled, (state, action) => {
+      state.me = action.payload
+    })
   },
 })
 
-export const { reset } = authSlice.actions
+export const { updateAccessToken, reset } = authSlice.actions
--- lib/slices/frogs.ts
+++ lib/slices/frogs.ts
@@ -1,9 +1,19 @@
-import { createSlice, SerializedError } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, SerializedError } from '@reduxjs/toolkit'
+import axios from '../axios'
 
 export enum FrogStates {
   IDLE = 'idle',
   LOADING = 'loading',
 }
+
+export const fetchFrogs = createAsyncThunk('auth/frogs', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get<{ hits: any[] }>('api/frogs')
+    return response
+  } catch (error) {
+    return thunkAPI.rejectWithValue({ error: error.message })
+  }
+})
 
 export interface AuthSliceState {
   accessToken: string
@@ -24,7 +34,19 @@ export const frogsSlice = createSlice({
   reducers: {
     reset: () => internalInitialState,
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchFrogs.fulfilled, (state, action) => {
+      state.frogs = [...action.payload.data.hits]
+      state.loading = FrogStates.IDLE
+    })
+    builder.addCase(fetchFrogs.rejected, (state, action) => {
+      state = { ...internalInitialState, error: action.error }
+      throw new Error(action.error.message)
+    })
+    builder.addCase(fetchFrogs.pending, (state) => {
+      state.loading = FrogStates.LOADING
+    })
+  },
 })
 
 export const { reset } = frogsSlice.actions
