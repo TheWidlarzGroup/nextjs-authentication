9ad208a3726d7b65586c84c96c563ae7ecb012ec
Nsquik <46610139+Nsquik@users.noreply.github.com>
2021-03-04 17:10:18 +0100

    Fix types

--- lib/store.ts
+++ lib/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { configureStore, combineReducers, AnyAction } from '@reduxjs/toolkit'
 import { createWrapper, MakeStore, HYDRATE } from 'next-redux-wrapper'
 import { authSlice } from './slices/auth'
 import { frogsSlice } from './slices/frogs'
@@ -7,8 +7,9 @@ const combinedReducers = combineReducers({
   authReducer: authSlice.reducer,
   frogsReducer: frogsSlice.reducer,
 })
+export type OurStore = ReturnType<typeof combinedReducers>
 
-const rootReducer = (state, action) => {
+const rootReducer = (state: ReturnType<typeof combinedReducers>, action: AnyAction) => {
   if (action.type === HYDRATE) {
     const nextState = {
       ...state,
@@ -19,12 +20,11 @@ const rootReducer = (state, action) => {
   return combinedReducers(state, action)
 }
 
-export const store = configureStore<typeof combineReducers>({
+export const store = configureStore<OurStore>({
   reducer: rootReducer,
 })
-
 const makeStore: MakeStore = () => store
+
 export const wrapper = createWrapper(makeStore, { storeKey: 'key' })
 
-export type OurStore = ReturnType<typeof store.getState>
 export type ThunkDispatch = typeof store.dispatch
